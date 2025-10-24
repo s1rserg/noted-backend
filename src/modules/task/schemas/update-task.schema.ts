2ef2168145deb9e351a7c82075f32076d13f418e
s1rserg <sirserg05@gmail.com>
2025-10-24 10:57:14 +0300

    fix(update-task-schema): add deadline transform to update task schema

--- src/modules/task/schemas/update-task.schema.ts
+++ src/modules/task/schemas/update-task.schema.ts
@@ -1,4 +1,15 @@
 import { z } from 'zod';
 import { CreateTaskSchema } from './create-task.schema.js';
 
-export const UpdateTaskSchema = z.strictObject({}).extend(CreateTaskSchema.shape).partial();
+export const UpdateTaskSchema = z
+  .strictObject({})
+  .extend(CreateTaskSchema.shape)
+  .partial()
+  .omit({ deadline: true })
+  .extend({
+    deadline: z
+      .string()
+      .nullable()
+      .optional()
+      .transform((val) => (val === '' ? null : val)),
+  });
